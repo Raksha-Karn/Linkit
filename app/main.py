7791fb86859bcfedae98e3f411168b6ccc1c3ae5
Raksha Karn <karnraksha41@gmail.com>
2026-05-21 14:50:50 +0545

    chore(cors): add cors middleware

--- app/main.py
+++ app/main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI
 from .routers import users, urls
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(title="URL Shortener", version="1.0.0")
 app.include_router(users.router)
 app.include_router(urls.router)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def health_check():
