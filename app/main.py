31fd71a26a52cac6580966d453a68a2027b189e4
Raksha Karn <karnraksha41@gmail.com>
2026-05-21 12:20:50 +0545

    feat(main): create fastapi app

--- app/main.py
+++ app/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from url-shortener!")
+from fastapi import FastAPI
+from .database import Base, engine
+from .routers import users, urls
 
+Base.metadata.create_all(bind=engine)
 
-if __name__ == "__main__":
-    main()
+app = FastAPI(title="URL Shortener", version="1.0.0")
+app.include_router(users.router)
+app.include_router(urls.router)
+
+@app.get("/")
+def health_check():
+    return {"status": "ok"}
