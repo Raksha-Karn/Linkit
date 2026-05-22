7acf984892d191b08df2011fbc166019608823e5
Raksha Karn <karnraksha41@gmail.com>
2026-05-22 15:23:17 +0545

    fix(redis): update redis url

--- app/routers/urls.py
+++ app/routers/urls.py
@@ -12,9 +12,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-r = redis.Redis(
-    host=os.getenv("REDIS_HOST", "redis"),
-    port=int(os.getenv("REDIS_PORT", 6379)),
+r = redis.Redis.from_url(
+    url=os.getenv("REDIS_URL"),
     decode_responses=True
 )
 
